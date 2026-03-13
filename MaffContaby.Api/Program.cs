7dc7cb4941155d5ad80520ba9a04784d6cabe7cd
Edenilson maffezzoli <edenilson.maffezzoli@pulsati.com.br>
2026-03-13 17:31:15 -0300

    ajustes de  xlsx 2

--- MaffContaby.Api/Program.cs
+++ MaffContaby.Api/Program.cs
@@ -11,7 +11,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddCors(options =>
+builder.Services.AddCors(options => 
 {
     options.AddDefaultPolicy(policy =>
     {
