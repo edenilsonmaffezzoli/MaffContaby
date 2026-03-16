44f38eb77688ebed6d42c1ab1cc06cb6e422e55a
Edenilson maffezzoli <edenilson.maffezzoli@pulsati.com.br>
2026-03-16 09:10:05 -0300

    ajustes relatório

--- MaffContaby.Web/src/routers/app.routes.tsx
+++ MaffContaby.Web/src/routers/app.routes.tsx
@@ -2,6 +2,7 @@ import { Layout } from '@/components/layout';
 import { FinancasPage } from '@/pages/financas';
 import { ImportarPage } from '@/pages/importar';
 import { MovimentacoesPage } from '@/pages/movimentacoes';
+import { RelatoriosPage } from '@/pages/relatorios';
 import { Navigate, Route, Routes } from 'react-router-dom';
 
 export function AppRoutes() {
@@ -10,10 +11,10 @@ export function AppRoutes() {
       <Route element={<Layout />}>
         <Route index element={<MovimentacoesPage />} />
         <Route path="/financas" element={<FinancasPage />} />
+        <Route path="/relatorios" element={<RelatoriosPage />} />
         <Route path="/importar" element={<ImportarPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
   );
 }
-
